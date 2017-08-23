2f191d07d5097423b777c00e171241724f384bf5
NaderAbdelrahman <abdelrahmannader@gmail.com>
2017-08-23 11:46:35 -0400

    added shopping cart view

--- seeds/product-seeder.js
+++ seeds/product-seeder.js
@@ -7,40 +7,40 @@ mongoose.connect('mongodb://heroku_jnq5gcwg:augiuuk0t0lsaqla5e3kffa17k@ds155587.
 var products = [
     new Product({
         imagePath: 'https://i.pinimg.com/736x/25/2a/46/252a468c5a423a750167dbd424571310--paint-sample-art-paint-swatch-art.jpg',
-        title: 'Abstract Art',
+        title: 'Color Crystals',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequuntur debitis delectus deserunt dolore dolorem eum expedita, inventore nobis omnis perferendis possimus quas repellat soluta, sunt voluptate, voluptates! Assumenda, nulla?',
         price: 97
     }),
     new Product({
         imagePath: 'https://i.pinimg.com/originals/b2/97/50/b29750d569c9e8516af1c2a357ad1af4.jpg',
-        title: 'Abstract Art',
+        title: 'Rainbow Diamonds',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequuntur debitis delectus deserunt dolore dolorem eum expedita, inventore nobis omnis perferendis possimus quas repellat soluta, sunt voluptate, voluptates! Assumenda, nulla?',
-        price: 999
+        price: 453
     }),
     new Product({
         imagePath: 'https://i.pinimg.com/736x/25/2a/46/252a468c5a423a750167dbd424571310--paint-sample-art-paint-swatch-art.jpg',
-        title: 'Abstract Art',
+        title: 'Paint Shore',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequuntur debitis delectus deserunt dolore dolorem eum expedita, inventore nobis omnis perferendis possimus quas repellat soluta, sunt voluptate, voluptates! Assumenda, nulla?',
-        price: 197
+        price: 654
     }),
     new Product({
         imagePath: 'https://i.pinimg.com/originals/b2/97/50/b29750d569c9e8516af1c2a357ad1af4.jpg',
-        title: 'Abstract Art',
+        title: 'Color Storm',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequuntur debitis delectus deserunt dolore dolorem eum expedita, inventore nobis omnis perferendis possimus quas repellat soluta, sunt voluptate, voluptates! Assumenda, nulla?',
-        price: 999
+        price: 345
     }),
     new Product({
         imagePath: 'https://i.pinimg.com/736x/25/2a/46/252a468c5a423a750167dbd424571310--paint-sample-art-paint-swatch-art.jpg',
-        title: 'Abstract Art',
+        title: 'Sunny Ocean',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequuntur debitis delectus deserunt dolore dolorem eum expedita, inventore nobis omnis perferendis possimus quas repellat soluta, sunt voluptate, voluptates! Assumenda, nulla?',
         price: 399
     }),
 
     new Product({
         imagePath: 'https://i.pinimg.com/originals/b2/97/50/b29750d569c9e8516af1c2a357ad1af4.jpg',
-        title: 'Abstract Art',
+        title: 'Color Compass',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequuntur debitis delectus deserunt dolore dolorem eum expedita, inventore nobis omnis perferendis possimus quas repellat soluta, sunt voluptate, voluptates! Assumenda, nulla?',
-        price: 999
+        price: 135
     })
 ];
 
