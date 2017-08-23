1c66ec4c99832226fbe8af4049a51746efa04a97
NaderAbdelrahman <abdelrahmannader@gmail.com>
2017-08-22 22:28:09 -0400

    Fixed Cart, and added a badge

--- routes/index.js
+++ routes/index.js
@@ -15,13 +15,13 @@ router.get('/', function(req, res, next) {
     });
 });
 
-router.get('/add-to-cart/:id', function (req, res, next) {
+router.get('/add-to-cart/:id', function (req, res) {
   var productId = req.params.id;
-  var cart = new Cart(req.session.cart ? req.session : {items: {}});
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
 
   Product.findById(productId, function (err, product) {
       if (err){
-        return res.redirect('/');
+          return res.redirect('/');
       }
       cart.add(product, product.id);
       req.session.cart = cart;
